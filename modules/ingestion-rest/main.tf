969742c8d1924f3a1918ab14ff5531557353c2cf
enkumamoto <eiji.kumamoto@gmail.com>
2024-09-13 14:23:17 -0300

    novos arquivos

--- modules/ingestion-rest/main.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  tags            = merge(var.tags_to_append, { Environment = var.environment })
-  api_domain_name = "api.${var.environment}.${data.aws_route53_zone.dns.name}"
-  api_v2_domain_name = "api-v2.${var.environment}.${data.aws_route53_zone.dns.name}"
-}
